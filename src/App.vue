a371159a1ea3eb6d1c92c59fc683011b7de0ea5a
suhwa98 <tnghk1998@gmail.com>
2022-03-13 15:45:45 +0900

    첫 업데이트

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+
+
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>{{ 원룸들[누른거].title }}</h4>
+      <img :src="원룸들[누른거].image" alt="" class="room-img">
+      <p>{{ 원룸들[누른거].content }}</p>
+      <p>{{ 원룸들[누른거].price }} 원</p>
+      <button @click="모달창열렸니 = false">닫기</button>
+    </div>
+  </div>
+
+  <div class="menu">
+  <a v-for="a in 메뉴들" :key="a">{{ a }}</a>
+  </div>
+
+  <div v-for="(a,i) in 원룸들" :key="i">
+    <img :src="a.image" alt="" class="room-img">
+    <h4 @click = "모달창열렸니 = true; 누른거 = i ">{{a.title}}</h4>
+    <p>{{a.price}}원</p>
+  </div>
+</template>
+
+<script>
+
+import data from './assets/oneroom.js'; //변수명 자유(동일하지않아도됨)
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      누른거 : 0,
+      원룸들 : data,
+      모달창열렸니 : false,
+      신고수 : [0,0,0],
+      메뉴들 : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸','천호동원룸','마포구원룸'],
+    }
+  },
+  methods : {
+    increasd(){
+        this.신고수++
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style>
+body {
+  margin:0;
+}
+.black-bg {
+  width:100%; height:100%;
+  background:rgba(0,0,0,0.5);
+  position:fixed; padding:20px;
+}
+.white-bg {
+  width:100%; background:white;
+  border-radius:8px;
+  padding:20px;
+}
+
+.room-img {
+  width:100%;
+  margin-top:40px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+.menu{
+  background:darkslateblue;
+  padding:15px;
+  border-radius:5px;
+}
+.menu a{
+  color:white;
+  padding:10px;
+}
+</style>
